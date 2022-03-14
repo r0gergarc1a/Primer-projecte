97b4369f82e9354f0600877af5be35a34b48f231
Roger Garcia <101257233+r0gergarc1a@users.noreply.github.com>
2022-03-14 14:26:29 +0100

    14/03/2022
    Creacio area2d, moure amb tecles

--- Sprite.gd
+++ Sprite.gd
@@ -4,7 +4,9 @@ extends Sprite
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var velocitat = Vector2(150,150)
+export var velocitat = 300
+var direccio = Vector2.ZERO
+	
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,14 +17,17 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	position += velocitat * delta
-	rotation_degrees += 90 * delta
-	if position.x >= 1024 or position.x <= 0:
-		velocitat.x = -velocitat.x
-	if position.y <= 0 or position.y >= 600:
-		velocitat.y = -velocitat.y
-	if $Fill.position.x == 100:
-		$Fill.position.y += 1
-	if $Fill.position.y == 100:
-		$Fill.position.x -= 1
+	direccio = Vector2.ZERO
+	
+	if Input.is_action_pressed("mou dreta"):
+		direccio += Vector2(1, 0)
+	if Input.is_action_pressed("mou esquerra"):
+		direccio += Vector2(-1, 0)
+	if Input.is_action_pressed("mou dalt"):
+		direccio += Vector2(0, -1)
+	if Input.is_action_pressed("mou baix"):
+		direccio += Vector2(0, 1)
+	
+		
+	position += velocitat * delta * direccio.normalized()
 	
